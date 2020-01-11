9161d155bac9376715110eb09d4ae4c7f4e74451
Jack Parker <jprkr96@gmail.com>
2020-01-11 18:37:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.merge![:yukihiro_matsumoto] = {
+programmer_hash.merge!(:yukihiro_matsumoto = {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
- }
+ )
     
 end
 
